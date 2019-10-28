79fbd8a8de3d0738111aa4b24a2be00568b4cf1c
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-10-28 16:02:57 +0300

    add showing jpg images

--- tst.zzz.com.ua/index.php
+++ tst.zzz.com.ua/index.php
@@ -16,6 +16,18 @@ foreach (glob("*.png") as $filename) {
 
 }
 
+foreach (glob("*.jpg") as $filename) {
+    //echo "$filename size " . filesize($filename) . "\n";
+ //echo '<p><a href="delete_image.php" name="image"><img src="'.$filename.'" alt="key"></p>';
+
+ echo '<form id="delimage" name="delimage" action="delete_image.php">
+          <input type="hidden" id="image_id" name="image_id" value="'.$filename.'">
+           <input type="image" name="submit" src="'.$filename.'" alt="Submit" />
+      </form>';
+
+
+}
+
 echo '<form enctype="multipart/form-data" name="upload_bage" id="upload_bage" action="upload.php" method="POST">
         <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
         <input name="userfile" type="file" />
