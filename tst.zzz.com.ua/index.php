aec7eedcfdbca6fab81db845e0c40094a06dc5d6
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-10-28 20:44:38 +0300

    test could create bages

--- tst.zzz.com.ua/index.php
+++ tst.zzz.com.ua/index.php
@@ -20,11 +20,12 @@ foreach (glob("*.jpg") as $filename) {
     //echo "$filename size " . filesize($filename) . "\n";
  //echo '<p><a href="delete_image.php" name="image"><img src="'.$filename.'" alt="key"></p>';
 
- echo '<form id="delimage" name="delimage" action="delete_image.php">
+ /*echo '<form id="delimage" name="delimage" action="delete_image.php">
           <input type="hidden" id="image_id" name="image_id" value="'.$filename.'">
            <input type="image" name="submit" src="'.$filename.'" alt="Submit" />
       </form>';
-
+*/
+echo '<p><a href=""><img src="'.$filename.'" width="200" height="100"></p>';
 
 }
 
@@ -34,8 +35,71 @@ echo '<form enctype="multipart/form-data" name="upload_bage" id="upload_bage" ac
         <input type="submit" value="Отправить файл" />
       </form>';
 
+//****************
 
- ?>
+
+// исходное изображение
+$img="banan.png";
+
+// imagecreatefrompng - создаёт новое изображение из файла или URL
+// водяной знак (шаблон бейджа)
+$wm=imagecreatefrompng('bage2.png');
+
+// imagesx - получает ширину изображения
+$wmW=imagesx($wm);
+
+// imagesy - получает высоту изображения
+$wmH=imagesy($wm);
+
+// imagecreatetruecolor - создаёт новое изображение true color
+$image=imagecreatetruecolor($wmW, $wmH);
+
+// выясняем расширение изображения на которое будем накладывать водяной знак
+if(preg_match("/.gif/i",$img)):
+	$image=imagecreatefromgif($img);
+elseif(preg_match("/.jpeg/i",$img) or preg_match("/.jpg/i",$img)):
+	$image=imagecreatefromjpeg($img);
+elseif(preg_match("/.png/i",$img)):
+	$image=imagecreatefrompng($img);
+else:
+	die("Ошибка! Неизвестное расширение изображения");
+endif;
+// узнаем размер изображения
+$size=getimagesize($img);
+
+// указываем координаты, где будет располагаться водяной знак
+/*
+* $size[0] - ширина изображения
+* $size[1] - высота изображения
+* - 10 -это расстояние от границы исходного изображения
+*/
+$cx=$size[0]-$wmW-10;
+$cy=$size[1]-$wmH-10;
+
+/* imagecopyresampled - копирует и изменяет размеры части изображения
+* с пересэмплированием
+*/
+imagecopyresampled ($image, $wm, $cx, $cy, 0, 0, $wmW, $wmH, $wmW, $wmH);
+
+/* imagejpeg - создаёт JPEG-файл filename из изображения image
+* третий параметр - качество нового изображение
+* параметр является необязательным и имеет диапазон значений
+* от 0 (наихудшее качество, наименьший файл)
+* до 100 (наилучшее качество, наибольший файл)
+* По умолчанию используется значение по умолчанию IJG quality (около 75)
+*/
+imagejpeg($image,$img,90);
+
+// imagedestroy - освобождает память
+imagedestroy($image);
+
+imagedestroy($wm);
+
+// на всякий случай
+unset($image,$img);
+
+?>
+
 
 
 
---
+++ tst.zzz.com.ua/test.php
@@ -0,0 +1,25 @@
+<?php
+// файл
+
+$filename1 = 'bage.png';
+$filename2 = 'banan.png';
+$percent = 0.5;
+
+// тип содержимого
+header('Content-Type: image/jpeg');
+
+// получение новых размеров
+list($width, $height) = getimagesize($filename1);
+$new_width = $width;
+$new_height = $height;
+
+// ресэмплирование
+//$image_p = imagecreatetruecolor($new_width, $new_height);
+$image = imagecreatefrompng($filename1);
+$image_p = imagecreatefrompng($filename2);
+imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+// вывод
+imagejpeg($image_p, null, 100);
+
+?>
