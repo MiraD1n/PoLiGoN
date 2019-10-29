e9d9cf209106ad0d0e21ab6ca294cb943e8d4b11
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-10-29 11:14:26 +0300

    Correct showing

--- tst.zzz.com.ua/test.php
+++ tst.zzz.com.ua/test.php
@@ -14,7 +14,7 @@ $new_width = $width;
 $new_height = $height;
 
 // ресэмплирование
-//$image_p = imagecreatetruecolor($new_width, $new_height);
+$image_p = imagecreatetruecolor($new_width, $new_height);
 $image = imagecreatefrompng($filename1);
 $image_p = imagecreatefrompng($filename2);
 imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
