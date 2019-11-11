a6550bab5170574f90ef0483322a32e37d48f19f
Полищук Максим Юрьевич <mpolishchuk@just2trade.online>
2019-11-11 12:52:12 +0300

    you should see in positioning

--- tst.zzz.com.ua/index.php
+++ tst.zzz.com.ua/index.php
@@ -1,7 +1,22 @@
-<link rel="stylesheet" type="text/css" href="style.css" />
+<!--<link rel="stylesheet" type="text/css" href="style.css" />-->
 
-<figure class = sign>
-<a href= ""><img weight=130 height=130 src="87.png"></a>
+<style type="text/css">
+figure {border: 4px double black; width: 168; height: 168;}
+ .card IMG { position: relative;}
+ .pgn { left: -40px; top: -75px; }
+ .bg { left: 0px; top: 0px; }
+</style>
+
+<figure class = "card">
+<a href= ""><img weight=130 height=130 src="87.png" class = "bg"></a>
+<a href= ""><img weight=60 height=60 src="pgn.png" class ="pgn"></a>
+<figcaption>Name</figcaption>
+<a href= ""><img weight=15 height=15 src="bar.png"></a>
+</figure>
+
+<figure class = "card">
+<a href= ""><img weight=130 height=130 src="87.png" class = "bg"></a>
+<a href= ""><img weight=60 height=60 src="pgn.png" class ="pgn"></a>
 <figcaption>Name</figcaption>
 <a href= ""><img weight=15 height=15 src="bar.png"></a>
 </figure>
