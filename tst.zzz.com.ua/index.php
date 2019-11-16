9f58f4e2e7860d56225f85972ed072ed83c81c3d
MiraD1n <mupolischuk@gmail.com>
2019-11-16 11:34:14 +0200

    work

--- tst.zzz.com.ua/index.php
+++ tst.zzz.com.ua/index.php
@@ -1,5 +1,6 @@
 <!--<link rel="stylesheet" type="text/css" href="style.css" />-->
 
+<!--
 <style type="text/css">
 figure {border: 4px double black; width: 168; height: 168;}
  .card IMG { position: relative;}
@@ -9,14 +10,120 @@ figure {border: 4px double black; width: 168; height: 168;}
 
 <figure class = "card">
 <a href= ""><img weight=130 height=130 src="87.png" class = "bg"></a>
-<a href= ""><img weight=60 height=60 src="pgn.png" class ="pgn"></a>
+<a href= ""><img weight=60 height=60 src="87.png" class ="87"></a>
 <figcaption>Name</figcaption>
 <a href= ""><img weight=15 height=15 src="bar.png"></a>
 </figure>
 
-<figure class = "card">
-<a href= ""><img weight=130 height=130 src="87.png" class = "bg"></a>
-<a href= ""><img weight=60 height=60 src="pgn.png" class ="pgn"></a>
-<figcaption>Name</figcaption>
-<a href= ""><img weight=15 height=15 src="bar.png"></a>
+-->
+
+<!-- WORK -->
+<style>
+.ola{
+  position: relative;
+  width: 50px;
+  height: 20px;
+}
+.sign {
+text-align: center;
+float: left; /* Выравнивание по правому краю */
+border: 1px solid #333; /* Параметры рамки */
+padding: 7px; /* Поля внутри блока */
+margin: 10px 0 5px 5px; /* Отступы вокруг */
+/*background: #f0f0f0; /* Цвет фона */
+}
+img{
+  /*border: 1px solid #333; /* Параметры рамки */
+}
+/*.sign figcaption {
+margin: 0 auto 5px; /* Отступы вокруг абзаца */
+/*border: 1px solid #333; /* Параметры рамки
+text-align: center;
+font-size: 15px;
+}*/
+/*.move{
+  position: relative;
+  top: -20px;
+}*/
+header {
+display: block; /* Для старых браузеров */
+height: 120px; /* Высота шапки */
+background:url(pride.png) right top repeat-x;
+}
+#container {
+  width: 150px;
+  height: 190px;
+  position: relative;
+  margin: 0 auto 0px; /* Отступы вокруг абзаца */
+  /*border: 1px solid #333; /* Параметры рамки */
+  text-align: center;
+  font-size: 15px;
+}
+#navi {
+  /*border: 1px solid #333; /* Параметры рамки */
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+}
+#infoi {
+  /*border: 1px solid #333; /* Параметры рамки */
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 20;
+  left: 0;
+}
+</style>
+<!--
+<div class = ola>
+<figure class = sign>
+<div><a href= ""><img weight=50 height=50 src="87.png"></a></div>
+<div class = move><a href= ""><img weight=130 height=130 src="87.png"></a>
+<figcaption>NAME</figcaption>
+<a href= ""><img weight=15 height=15 src="bar.png"></a></div>
 </figure>
+</div>
+-->
+
+<div id="container">
+<figure>
+<div id="navi"><a href= ""><img weight=50 height=50 src="87.png"></a></div>
+<div id="infoi"><a href= ""><img weight=130 height=130 src="87.png"></a>
+<figcaption>NAME</figcaption>
+<a href= ""><img weight=15 height=15 src="bar.png"></a></div>
+</figure>
+</div>
+
+
+
+<!-- stackoverflow
+<style>
+#container {
+  width: 100px;
+  height: 100px;
+  position: relative;
+}
+#navi,
+#infoi {
+  border: 1px solid #333; /* Параметры рамки */
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+#infoi {
+  z-index: 10;
+}
+</style>
+
+<div id="container">
+  <div id="navi">a</div>
+  <div id="infoi">
+    <img src="https://appharbor.com/assets/images/stackoverflow-logo.png" height="200" width="320" />b
+  </div>
+</div>
+-->
